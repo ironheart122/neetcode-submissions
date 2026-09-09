bb843117838f9250372588d917a85e45b275b4f3
moncy <18900204+ironheart122@users.noreply.github.com>
2026-09-09 11:07:32 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.ts
@@ -0,0 +1,14 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums: number[]): boolean {
+    // if nums doesnt contain duplicates then its length will always be equal to the length of the set of distinct elements derived from nums
+
+    // O(n)
+    return nums.length !== new Set(nums).size
+  }
+}
