f32c9c0712f6c77492b3972c0fa10ede43ef52ab
moncy <18900204+ironheart122@users.noreply.github.com>
2026-09-24 13:26:53 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        // SO here im gonna just go through the array and for every element, we check if we've seen the diff of this and the target before,
+        // if so, we return the indices of the diff and the current element. We will use Set for this. The solution will run in O(n) time
+        
+        const seen = new Map<number, number>()
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i]
+
+            if (seen.has(diff)) {
+                return [seen.get(diff), i]
+            }
+
+            seen.set(nums[i], i)
+        }
+
+        return []
+    }
+}
